334c990f57dc6ac04725367b9021e227e86c157c
KellyBringino <57270541+KellyBringino@users.noreply.github.com>
2025-12-17 02:42:07 -0800

    added cowboy effects to the boots

--- src/main/java/net/dontouchat/cowboyhats/item/custom/CowboyBootItem.java
+++ src/main/java/net/dontouchat/cowboyhats/item/custom/CowboyBootItem.java
@@ -17,6 +17,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
@@ -30,10 +32,15 @@ import java.util.*;
 public class CowboyBootItem extends CowboyArmorItem {
     private Random random;
     private final float luckChance = 0.1f;
+    private final float travelDist = 500.0f;
+    private final int travelEXP = 5;
     public enum CowboyEffect{
         LUCKY,
         ARMORED,
-        TRAVELER
+        TRAVELER,
+        FASTRIDER,
+        ESCAPERIDER,
+        SPRINGY
     }
     private Map<CowboyEffect,String[]> effectmap = new HashMap();
     public CowboyBootItem(ArmorMaterial pMaterial, Type slot, Properties pProperties)
@@ -43,11 +50,21 @@ public class CowboyBootItem extends CowboyArmorItem {
         this.effectmap.put(CowboyEffect.LUCKY,new String[]{"lucky","Lucky"});
         this.effectmap.put(CowboyEffect.ARMORED,new String[]{"armored","Armored"});
         this.effectmap.put(CowboyEffect.TRAVELER,new String[]{"traveler","Traveler"});
+        this.effectmap.put(CowboyEffect.FASTRIDER,new String[]{"fastrider","Fast Rider"});
+        this.effectmap.put(CowboyEffect.ESCAPERIDER,new String[]{"escaperider","Escape Rider"});
+        this.effectmap.put(CowboyEffect.SPRINGY,new String[]{"springy","Springy"});
         this.random = new Random(System.currentTimeMillis());
     }
     public boolean tryLuck(ItemStack pStack){
         return pStack.getTag().getBoolean("cowboyhats." + effectmap.get(CowboyEffect.LUCKY)[0])
                 && random.nextFloat() <= luckChance;
+    }
+    public boolean tryTravel(ItemStack pStack){
+        return pStack.getTag().getBoolean("cowboyhats." + effectmap.get(CowboyEffect.TRAVELER)[0]);
+    }
+    public boolean tryEscape(LivingEntity entity, ItemStack pStack){
+        return pStack.getTag().getBoolean("cowboyhats." + effectmap.get(CowboyEffect.ESCAPERIDER)[0]) &&
+                entity.isPassenger();
     }
     private CompoundTag NBTBase(CompoundTag NBT){
         NBT.putInt("cowboyhats.tier",0);
@@ -122,8 +139,17 @@ public class CowboyBootItem extends CowboyArmorItem {
         @Override
     public void onInventoryTick(ItemStack stack, Level level, Player player, int slotIndex, int selectedIndex) {
         super.onInventoryTick(stack, level, player, slotIndex, selectedIndex);
-        if(slotIndex == 39){
+        if(slotIndex == 36){
+            if(player.isPassenger() && stack.getTag().getBoolean("cowboyhats." + effectmap.get(CowboyEffect.FASTRIDER)[0])){
+                Entity entity = player.getVehicle();
+                if(entity instanceof LivingEntity){
+                    ((LivingEntity)entity).addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,20));
+                }
+            }
 
+            if(stack.getTag().getBoolean("cowboyhats." + effectmap.get(CowboyEffect.SPRINGY)[0])){
+                player.addEffect(new MobEffectInstance(MobEffects.JUMP,20));
+            }
         }
     }
 
@@ -133,8 +159,43 @@ public class CowboyBootItem extends CowboyArmorItem {
         ItemStack boot = entity.getItemBySlot(EquipmentSlot.FEET);
         if (!boot.isEmpty() && boot.getItem() instanceof CowboyBootItem)
         {
+            if(((CowboyBootItem) boot.getItem()).tryEscape(entity,boot)){
+                ((LivingEntity) entity.getVehicle()).addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,200,2));
+            }
+
             if(((CowboyBootItem) boot.getItem()).tryLuck(boot)){
                 event.setCanceled(true);
+            }
+        }
+
+        if(entity.isVehicle()){
+            boot = ((LivingEntity) entity.getFirstPassenger()).getItemBySlot(EquipmentSlot.FEET);
+            if (!boot.isEmpty() && boot.getItem() instanceof CowboyBootItem){
+                if(((CowboyBootItem) boot.getItem()).tryEscape((LivingEntity) entity.getFirstPassenger(),boot)){
+                    entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,200,2));
+                }
+            }
+        }
+    }
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event){
+        Player player = event.player;
+        ItemStack boot = player.getItemBySlot(EquipmentSlot.FEET);
+        if(!boot.isEmpty() && boot.getItem() instanceof CowboyBootItem){
+            if(((CowboyBootItem) boot.getItem()).tryTravel(boot)){
+                float dist = boot.getTag().getFloat("cowboyhats.distance");
+                double deltaX = player.getX() - player.xOld;
+                double deltaY = player.getY() - player.yOld;
+                double deltaZ = player.getZ() - player.zOld;
+                double distMoved = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ));
+                dist += (float) distMoved;
+                if(dist >= ((CowboyBootItem) boot.getItem()).travelDist){
+                    player.giveExperiencePoints(((CowboyBootItem) boot.getItem()).travelEXP);
+                    dist = 0.0f;
+                }
+                CompoundTag tag = boot.getTag();
+                tag.putFloat("cowboyhats.distance",dist);
+                boot.setTag(tag);
             }
         }
     }
